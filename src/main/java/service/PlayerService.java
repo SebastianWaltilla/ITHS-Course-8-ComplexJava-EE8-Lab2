750f958280236969c43859bc0f79f64a7f38a672
sebas <sebastianwaltilla@hotmail.com>
2020-11-09 09:56:53 +0100

    Starting with lab with Payara

---
+++ src/main/java/service/PlayerService.java
@@ -0,0 +1,34 @@
+package service;
+import entity.PlayerData;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+public class PlayerService {
+
+    EntityManager entityManager;
+
+    public PlayerData createStudent(PlayerData player) {
+        entityManager.persist(player);
+        return player;
+    }
+
+    public PlayerData updateTodo(PlayerData player) {
+        entityManager.merge(player);
+        return player;
+    }
+
+    public PlayerData findStudentById(Long id) {
+        return entityManager.find(PlayerData.class, id);
+    }
+
+    public List<PlayerData> getAllStudents() {
+        return entityManager.createQuery("SELECT s from PlayerData s", PlayerData.class).getResultList();
+    }
+
+
+}
