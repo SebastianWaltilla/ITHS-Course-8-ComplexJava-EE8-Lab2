96a246a1246f1cad46e1c3a4c278eeedde072bfc
sebas <sebastianwaltilla@hotmail.com>
2020-11-20 00:18:15 +0100

    changed some data in generator to se if bindings is ok

--- src/main/java/dataGenerator/SchoolDataGenerator.java
+++ src/main/java/dataGenerator/SchoolDataGenerator.java
@@ -21,10 +21,11 @@ public class SchoolDataGenerator {
 
         Student student1 = new Student("Anton", "Månpuss", "Anton@Månpuss.nu", "031-010101");
         Student student2 = new Student( "Leo", "Lejon", "Leo@Lejon.nu", "0708-222333");
-        Student student3 = new Student("Mia", "Uhh", "mia@Uhh.me", "031-223311");
+        Student student3 = new Student("Mia", "Uhh", "mia@Think.me", "031-223311");
+        Student student4 = new Student("Li", "Fei", "Li@Fei.me", "0322-776655");
 
         Subject subject1 = new Subject("Java");
-        Subject subject2 = new Subject("C#");
+        Subject subject2 = new Subject("PHP");
         Subject subject3 = new Subject("Javascript");
 
         Teacher teacher1 = new Teacher("JohanSvensson");
@@ -40,6 +41,8 @@ public class SchoolDataGenerator {
         student2.addSubject(subject2);
         student3.addSubject(subject3);
 
+        student4.addSubject(subject3);
+
         teacher1.addSubject(subject1);
         teacher2.addSubject(subject2);
 
@@ -47,6 +50,7 @@ public class SchoolDataGenerator {
         entityManager.persist(student1);
         entityManager.persist(student2);
         entityManager.persist(student3);
+        entityManager.persist(student4);
         entityManager.persist(teacher1);
         entityManager.persist(teacher2);
         entityManager.persist(teacher3);
